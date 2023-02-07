586c3884a82c763715916563f9a3579e6c062620
NataliaFiryn <natalia.firyn@itmagination.com>
2023-02-07 14:24:27 +0100

    Datepicker updated, autocomplete and loader added, minor fixes in rest of files

--- cypress/e2e/ContactUs.cy.js
+++ cypress/e2e/ContactUs.cy.js
@@ -2,14 +2,14 @@ describe('Reset of entered data', () => {
   it('Reset of entered data', () => {
     cy.visit('https://webdriveruniversity.com/Contact-Us/contactus.html')
 
-    cy.get('form').then( form => {
-    cy.wrap(form).find('[name="first_name"]').click().type('Jane')
-    cy.wrap(form).find('[name="last_name"]').click().type('Doe')
-    cy.wrap(form).find('[name="email"]').click().type('Jane.Doe@example.com')
-    cy.wrap(form).find('[name="message"]').click().type('Comment')
+    cy.get('form').then(form => {
+      cy.wrap(form).find('[name="first_name"]').click().type('Jane')
+      cy.wrap(form).find('[name="last_name"]').click().type('Doe')
+      cy.wrap(form).find('[name="email"]').click().type('Jane.Doe@example.com')
+      cy.wrap(form).find('[name="message"]').click().type('Comment')
 
-    cy.get('#form_buttons').find('[type="reset"]').click()
-    
+      cy.get('#form_buttons').find('[type="reset"]').click()
+
       cy.wrap(form).find('[name="first_name"]').should('have.value', '')
       cy.wrap(form).find('[name="last_name"]').should('have.value', '')
       cy.wrap(form).find('[name="email"]').should('have.value', '')
@@ -23,86 +23,86 @@ describe('Partial filling up of the form', () => {
   it('Empty form', () => {
     cy.visit('https://webdriveruniversity.com/Contact-Us/contactus.html')
     cy.get('#form_buttons').find('[type="submit"]').click()
-    cy.get('body').then (validationMessage =>{
-    cy.wrap(validationMessage).should('contain','Error: all fields are required')
-    cy.wrap(validationMessage).should('contain','Error: Invalid email address')
+    cy.get('body').then(validationMessage => {
+      cy.wrap(validationMessage).should('contain', 'Error: all fields are required')
+      cy.wrap(validationMessage).should('contain', 'Error: Invalid email address')
     })
   })
- it('Field FirstName was not filled up', () => {
+  it('Field FirstName was not filled up', () => {
     cy.visit('https://webdriveruniversity.com/Contact-Us/contactus.html')
-    cy.get('form').then( form => {
+    cy.get('form').then(form => {
       cy.wrap(form).find('[name="last_name"]').click().type('Doe')
       cy.wrap(form).find('[name="email"]').click().type('Jane.Doe@example.com')
       cy.wrap(form).find('[name="message"]').click().type('Comment')
     })
     cy.get('#form_buttons').find('[type="submit"]').click()
-    cy.get('body').should('contain','Error: all fields are required')
+    cy.get('body').should('contain', 'Error: all fields are required')
   })
   it('Field LastName was not filled up', () => {
     cy.visit('https://webdriveruniversity.com/Contact-Us/contactus.html')
-    cy.get('form').then( form => {
+    cy.get('form').then(form => {
       cy.wrap(form).find('[name="first_name"]').click().type('Jane')
       cy.wrap(form).find('[name="email"]').click().type('Jane.Doe@example.com')
       cy.wrap(form).find('[name="message"]').click().type('Comment')
     })
     cy.get('#form_buttons').find('[type="submit"]').click()
-    cy.get('body').should('contain','Error: all fields are required')
+    cy.get('body').should('contain', 'Error: all fields are required')
   })
   it('Field Email was not filled up', () => {
     cy.visit('https://webdriveruniversity.com/Contact-Us/contactus.html')
-    cy.get('form').then( form => {
+    cy.get('form').then(form => {
       cy.wrap(form).find('[name="first_name"]').click().type('Jane')
       cy.wrap(form).find('[name="last_name"]').click().type('Doe')
       cy.wrap(form).find('[name="message"]').click().type('Comment')
     })
     cy.get('#form_buttons').find('[type="submit"]').click()
-    cy.get('body').then (validationMessage =>{
-      cy.wrap(validationMessage).should('contain','Error: all fields are required')
-      cy.wrap(validationMessage).should('contain','Error: Invalid email address')
-      })
+    cy.get('body').then(validationMessage => {
+      cy.wrap(validationMessage).should('contain', 'Error: all fields are required')
+      cy.wrap(validationMessage).should('contain', 'Error: Invalid email address')
+    })
   })
   it('Field Comments was not filled up', () => {
     cy.visit('https://webdriveruniversity.com/Contact-Us/contactus.html')
-    cy.get('form').then( form => {
+    cy.get('form').then(form => {
       cy.wrap(form).find('[name="first_name"]').click().type('Jane')
       cy.wrap(form).find('[name="last_name"]').click().type('Doe')
       cy.wrap(form).find('[name="email"]').click().type('Jane.Doe@example.com')
     })
     cy.get('#form_buttons').find('[type="submit"]').click()
-    cy.get('body').should('contain','Error: all fields are required')
+    cy.get('body').should('contain', 'Error: all fields are required')
   })
-  })
+})
 
-  describe('Email Adress validation', () => {
+describe('Email Adress validation', () => {
 
-    it('Invalid Email Adresses', () => {
-      
-      const invalidEmail = ['@example.com', 'JaneDoe', 'Jane.Doeexample.com', 'Jane.Doe@', 'Jane..Doeexample.com', 'Jane@Doe@111.222.333.44444']
-      
-      cy.wrap(invalidEmail).each (invalidEmail => {
+  it('Invalid Email Adresses', () => {
+
+    const invalidEmail = ['@example.com', 'JaneDoe', 'Jane.Doeexample.com', 'Jane.Doe@', 'Jane..Doeexample.com', 'Jane@Doe@111.222.333.44444']
+
+    cy.wrap(invalidEmail).each(invalidEmail => {
       cy.visit('https://webdriveruniversity.com/Contact-Us/contactus.html')
       cy.get('form').find('[name="first_name"]').click().type('Jane')
       cy.get('form').find('[name="last_name"]').click().type('Doe')
       cy.get('form').find('[name="email"]').click().type(invalidEmail)
       cy.get('form').find('[name="message"]').click().type('Comment')
       cy.get('#form_buttons').find('[type="submit"]').click()
-      
-      cy.get('body').should('contain','Error: Invalid email address')
-    })
+
+      cy.get('body').should('contain', 'Error: Invalid email address')
     })
   })
+})
 
-  describe('Happy path', () => {
-    it('All fields filled up correct', () => {
-      cy.visit('https://webdriveruniversity.com/Contact-Us/contactus.html')
-  
-      cy.get('form').then( form => {
+describe('Happy path', () => {
+  it('All fields filled up correct', () => {
+    cy.visit('https://webdriveruniversity.com/Contact-Us/contactus.html')
+
+    cy.get('form').then(form => {
       cy.wrap(form).find('[name="first_name"]').click().type('Jane')
       cy.wrap(form).find('[name="last_name"]').click().type('Doe')
       cy.wrap(form).find('[name="email"]').click().type('Jane.Doe@example.com')
       cy.wrap(form).find('[name="message"]').click().type('Comment')
-      })
-      cy.get('#form_buttons').find('[type="submit"]').click()
-      cy.get('body').should('contain', 'Thank You for your Message!')
     })
+    cy.get('#form_buttons').find('[type="submit"]').click()
+    cy.get('body').should('contain', 'Thank You for your Message!')
   })
+})
--- cypress/e2e/Datepicker-AutocompleteTextField-AjaxLoader.cy.js
+++ cypress/e2e/Datepicker-AutocompleteTextField-AjaxLoader.cy.js
@@ -1,19 +1,65 @@
 describe('Datapicker', () => {
     it('Current date', () => {
-        let date = new Date()
-        let day = date.getDate()
-        let month = date.toLocaleDateString('default', {month: 'long'})
-        let year = date.getFullYear()
-        let todayMonthAndYear = month + ' ' + year
-        
-    cy.visit('https://webdriveruniversity.com/Datepicker/index.html')
-    cy.get('.form-control').click()
-    cy.get('thead tr').find('th').eq(1).should('contain', todayMonthAndYear)
-    cy.get('tbody tr').find('[class="today day"]').should('contain', day)
+
+        let currentDate = new Date().toLocaleDateString('en-US', { year: 'numeric', month: '2-digit', day: '2-digit' }).replaceAll('/', '-')
+        cy.visit('https://webdriveruniversity.com/Datepicker/index.html')
+        cy.get('input').invoke('prop', 'value').should('contain', currentDate)
     })
 
     it('Provide date', () => {
+        let date = new Date()
+        date.setDate(date.getDate() + 90)
+        let futureDay = date.getDate()
+        let futureMonth = date.toLocaleDateString('default', { month: 'long' })
+
         cy.visit('https://webdriveruniversity.com/Datepicker/index.html')
-    
+        cy.get('.form-control').click()
+        selectDayFromCurret()
+        function selectDayFromCurret() {
+            cy.get('.datepicker-switch').invoke('prop', 'textContent').then(monthYear => {
+                if (!monthYear.includes(futureMonth)) {
+                    cy.get('[class="next"]').eq(0).click()
+                    selectDayFromCurret()
+                } else {
+                    cy.get('tbody tr').find('[class="day"]').contains(futureDay).click()
+                }
+            })
+        }
+        console.log(date)
+        let futureDate = date.toLocaleDateString('en-US', { year: 'numeric', month: '2-digit', day: '2-digit' }).replaceAll('/', '-')
+        cy.get('input').invoke('prop', 'value').should('contain', futureDate)
+    })
+})
+
+describe('Autocomplete TextField', () => {
+    it('Corectess of autocomplete', () => {
+        let inputText = ['I', 'Ba', 'Gra']
+        cy.visit('https://webdriveruniversity.com/Autocomplete-TextField/autocomplete-textfield.html')
+        cy.wrap(inputText).each(inputText => {
+            cy.get('#myInput').click().clear().type(inputText)
+            cy.get('#myInputautocomplete-list').find('strong').should('contain', inputText)
+        })
+    })
+    it('Check second element from autocomplete list', () => {
+        cy.visit('https://webdriveruniversity.com/Autocomplete-TextField/autocomplete-textfield.html')
+        cy.get('#myInput').click().type('Gra')
+        cy.get('#myInputautocomplete-list').find('strong').eq(1).should('contain', 'Gra')
+    })
+})
+
+describe('Ajax-Loader', () => {
+    it('Click me', () => {
+        cy.on('uncaught:exception', (err, runnable) => {
+            if (err.message.includes('Unexpected token')) {
+                console.log('Application Error Javascript Token')
+                return false
+            }
+            return true
+        })
+        cy.visit('https://webdriveruniversity.com/Ajax-Loader/index.html')
+
+        cy.get('[class="container"]').find('[style="display: none;"]', { timeout: 7000 })
+        cy.get('#button1').click()
+        cy.get('[class="modal-title"]').should('contain', 'Well Done For Waiting....!!!')
     })
 })
