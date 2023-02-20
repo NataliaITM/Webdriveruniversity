7b3d85b8629248572412efb33a6fb7eccc95c002
NataliaFiryn <natalia.firyn@itmagination.com>
2023-02-20 11:18:09 +0100

    Dropdown Checkbox and Buttons refactor

--- cypress/e2e/Dropdown-Checkboxes-RadioButtons.cy.js
+++ cypress/e2e/Dropdown-Checkboxes-RadioButtons.cy.js
@@ -7,15 +7,25 @@ describe('Dropdown Menu(s)', () => {
       })
 
     it('First dropdown', () => {
-        onDropdownCheckboxesRadiobuttonsPage.selectAllOptionsInFirstDropdownLists()
+        onDropdownCheckboxesRadiobuttonsPage.selectOptionFirstDropdownList('JAVA')
+        onDropdownCheckboxesRadiobuttonsPage.selectOptionFirstDropdownList('C#')
+        onDropdownCheckboxesRadiobuttonsPage.selectOptionFirstDropdownList('Python')
+        onDropdownCheckboxesRadiobuttonsPage.selectOptionFirstDropdownList('SQL')
+        onDropdownCheckboxesRadiobuttonsPage.checkNuberOfOptionsFirstDropdownList(4)
     })
-    
     it('Second dropdown', () => {
-        onDropdownCheckboxesRadiobuttonsPage.selectAllOptionsInSecondDropdownLists()
-    })
-    
+        onDropdownCheckboxesRadiobuttonsPage.selectOptionSecondDropdownList('Eclipse')
+        onDropdownCheckboxesRadiobuttonsPage.selectOptionSecondDropdownList('Maven')
+        onDropdownCheckboxesRadiobuttonsPage.selectOptionSecondDropdownList('TestNG')
+        onDropdownCheckboxesRadiobuttonsPage.selectOptionSecondDropdownList('JUnit')
+        onDropdownCheckboxesRadiobuttonsPage.checkNuberOfOptionsSecondDropdownList(4)
+    })  
     it('Third dropdown', () => {
-        onDropdownCheckboxesRadiobuttonsPage.selectAllOptionsInThirdDropdownLists()
+        onDropdownCheckboxesRadiobuttonsPage.selectOptionThirdDropdownList('HTML')
+        onDropdownCheckboxesRadiobuttonsPage.selectOptionThirdDropdownList('CSS')
+        onDropdownCheckboxesRadiobuttonsPage.selectOptionThirdDropdownList('JavaScript')
+        onDropdownCheckboxesRadiobuttonsPage.selectOptionThirdDropdownList('JQuery')
+        onDropdownCheckboxesRadiobuttonsPage.checkNuberOfOptionsThirdDropdownList(4)
     })
 })
 
@@ -24,15 +34,17 @@ describe('Checkboxe(s)', () => {
         navigateTo.dropdown_checkoxes_radiobuttonsPage()
       })
     
-    it("Chceck and uncheck all checkboxe(s)", () => {
-        onDropdownCheckboxesRadiobuttonsPage.defaultCheckOfCheckboxes()
-        onDropdownCheckboxesRadiobuttonsPage.checkAllCheckboxes()
-        onDropdownCheckboxesRadiobuttonsPage.uncheckAllCheckboxes()
+    it("Chceck and uncheck each checkbox)", () => {
+        onDropdownCheckboxesRadiobuttonsPage.checkAndUncheckCheckBox('option-1')
+        onDropdownCheckboxesRadiobuttonsPage.checkAndUncheckCheckBox('option-2')
+        onDropdownCheckboxesRadiobuttonsPage.checkAndUncheckCheckBox('option-3')
+        onDropdownCheckboxesRadiobuttonsPage.checkAndUncheckCheckBox('option-4')
+        onDropdownCheckboxesRadiobuttonsPage.checkNuberOfCheckboxes(4)
     })
     
     it('Check all checkboxe(s) and uncheck selected checkboxes', () => {
         onDropdownCheckboxesRadiobuttonsPage.checkAllCheckboxes()
-        onDropdownCheckboxesRadiobuttonsPage.uncheckTwoCheckboxes(['1','3'])
+        onDropdownCheckboxesRadiobuttonsPage.uncheckSelectedCheckboxes(['option-2','option-4'])
     })
 })
 
@@ -42,23 +54,32 @@ describe('Radio Button(s)', () => {
       })
 
     it('Click and check every radio button', () => {
-        onDropdownCheckboxesRadiobuttonsPage.checkedPropertyCheckForAllRadioButtons(false)
-        onDropdownCheckboxesRadiobuttonsPage.checkedPropertyCheckForAllRadioButtons(true)
+        onDropdownCheckboxesRadiobuttonsPage.clickRadioButton('green')
+        onDropdownCheckboxesRadiobuttonsPage.clickRadioButton('blue')
+        onDropdownCheckboxesRadiobuttonsPage.clickRadioButton('yellow')
+        onDropdownCheckboxesRadiobuttonsPage.clickRadioButton('orange')
+        onDropdownCheckboxesRadiobuttonsPage.clickRadioButton('purple')
+        onDropdownCheckboxesRadiobuttonsPage.checkNuberOfRadioButtons(5)
     })
 })
 
-describe('Selected & Disabled window', () => {
+describe('Selected & Disabled', () => {
     beforeEach('open Dropdown Menu(s), Checkboxe(s) & Radio Button(s) page', () => {
         navigateTo.dropdown_checkoxes_radiobuttonsPage()
       })
 
-    it('Default check of radio buttons', () => {
-        onDropdownCheckboxesRadiobuttonsPage.defaultCheckVegetableRadioButtons()
+    it('Vegetable Radio Buttons', () => {
+        onDropdownCheckboxesRadiobuttonsPage.clickVegetableRadiobuttons('pumpkin')
+        onDropdownCheckboxesRadiobuttonsPage.clickVegetableRadiobuttons('lettuce')
+        onDropdownCheckboxesRadiobuttonsPage.clickVegetableRadiobuttons('cabbage')
+        onDropdownCheckboxesRadiobuttonsPage.checkNuberOfOptionsVegetableRadioButtons(3)
     })
-    it('Check every radio button', () => {
-        onDropdownCheckboxesRadiobuttonsPage.clickOnAllVegetableRadioButtons()
-    })
+    
     it('Fruits dropdown', () => {
-        onDropdownCheckboxesRadiobuttonsPage.selectAllOptionsInSelectedDisabledDropdownLists()
+        onDropdownCheckboxesRadiobuttonsPage.selectOptionFruitDropdownList('Apple')
+        onDropdownCheckboxesRadiobuttonsPage.selectOptionFruitDropdownList('Orange')
+        onDropdownCheckboxesRadiobuttonsPage.selectOptionFruitDropdownList('Pear')
+        onDropdownCheckboxesRadiobuttonsPage.selectOptionFruitDropdownList('Grape')
+        onDropdownCheckboxesRadiobuttonsPage.checkNuberOfOptionsFruitDropdownList(4)
     })
 })
--- cypress/support/page_objects/dropdown_checkoxes_radiobuttonsPage.js
+++ cypress/support/page_objects/dropdown_checkoxes_radiobuttonsPage.js
@@ -1,70 +1,68 @@
 export class dropdownCheckboxesRadiobuttonsPage{
-    javaDropdownList = '#dropdowm-menu-1'
-    eclipseDropdownList = '#dropdowm-menu-2'
-    htmlDropdownList = '#dropdowm-menu-3'
-    fruitDropdownList = '#fruit-selects'
+    javaDropdownList = '.container .thumbnail #dropdowm-menu-1'
+    eclipseDropdownList = '.container .thumbnail #dropdowm-menu-2'
+    htmlDropdownList = '.container .thumbnail #dropdowm-menu-3'
+    fruitDropdownList = '.container .thumbnail #fruit-selects'
+    radioButtonsContainer = '.container .thumbnail #radio-buttons'
+    checkBoxesContainer = '.container .thumbnail #checkboxes'
+    selectedDisabledRadioButtons = '.container .thumbnail #radio-buttons-selected-disabled'
 
-
-    selectAllOptionsInFirstDropdownLists(){
-        cy.get('[class="container"]').contains('.thumbnail', 'Dropdown Menu(s)').find(onDropdownCheckboxesRadiobuttonsPage.javaDropdownList).find('option').each(dropdownItem => {
-            const itemName = dropdownItem.text()
+    //Dropdown Listy 
+    selectOptionFirstDropdownList(itemName){
+        cy.get(onDropdownCheckboxesRadiobuttonsPage.javaDropdownList).find('option').contains(itemName).then(dropdownItem =>{
             const itemValue = {
                 "JAVA": "java",
                 "C#": "c#",
                 "Python": "python",
                 "SQL": "sql"
             }
-            cy.get('[class="container"]').contains('.thumbnail', 'Dropdown Menu(s)').find(onDropdownCheckboxesRadiobuttonsPage.javaDropdownList).select(itemName)
+            cy.get(onDropdownCheckboxesRadiobuttonsPage.javaDropdownList).select(itemName)
             cy.wrap(dropdownItem).invoke('prop', 'selected').should('equal', true)
             cy.wrap(dropdownItem).should('contain', itemName)
             cy.wrap(dropdownItem).invoke('prop', 'value').should('contain', itemValue[itemName])
         })
     }
-    selectAllOptionsInSecondDropdownLists(){
-        cy.get('[class="container"]').contains('.thumbnail', 'Dropdown Menu(s)').find(onDropdownCheckboxesRadiobuttonsPage.eclipseDropdownList).find('option').each(dropdownItem => {
-            const itemName = dropdownItem.text()
+    selectOptionSecondDropdownList(itemName){
+        cy.get(onDropdownCheckboxesRadiobuttonsPage.eclipseDropdownList).find('option').contains(itemName).then(dropdownItem =>{
             const itemValue = {
                 "Eclipse": "eclipse",
                 "Maven": "maven",
                 "TestNG": "testng",
                 "JUnit": "junit"
             }
-            cy.get('[class="container"]').contains('.thumbnail', 'Dropdown Menu(s)').find(onDropdownCheckboxesRadiobuttonsPage.eclipseDropdownList).select(itemName)
+            cy.get(onDropdownCheckboxesRadiobuttonsPage.eclipseDropdownList).select(itemName)
             cy.wrap(dropdownItem).invoke('prop', 'selected').should('equal', true)
             cy.wrap(dropdownItem).should('contain', itemName)
             cy.wrap(dropdownItem).invoke('prop', 'value').should('contain', itemValue[itemName])
         })
     }
-    selectAllOptionsInThirdDropdownLists(){
-        cy.get('[class="container"]').contains('.thumbnail', 'Dropdown Menu(s)').find(onDropdownCheckboxesRadiobuttonsPage.htmlDropdownList).find('option').each(dropdownItem => {
-            const itemName = dropdownItem.text()
+    selectOptionThirdDropdownList(itemName){
+        cy.get(onDropdownCheckboxesRadiobuttonsPage.htmlDropdownList).find('option').contains(itemName).then(dropdownItem =>{
             const itemValue = {
                 "HTML": "html",
                 "CSS": "css",
                 "JavaScript": "javascript",
                 "JQuery": "jquery"
             }
-            cy.get('[class="container"]').contains('.thumbnail', 'Dropdown Menu(s)').find(onDropdownCheckboxesRadiobuttonsPage.htmlDropdownList).select(itemName)
+            cy.get(onDropdownCheckboxesRadiobuttonsPage.htmlDropdownList).select(itemName)
             cy.wrap(dropdownItem).invoke('prop', 'selected').should('equal', true)
             cy.wrap(dropdownItem).should('contain', itemName)
             cy.wrap(dropdownItem).invoke('prop', 'value').should('contain', itemValue[itemName])
         })
     }
-
-    selectAllOptionsInSelectedDisabledDropdownLists(){
-        cy.get('[class="container"]').contains('.thumbnail', 'Selected & Disabled').find(onDropdownCheckboxesRadiobuttonsPage.fruitDropdownList).find('option').each((fruit, index) => {
-            const fruitName = fruit.text()
+    selectOptionFruitDropdownList(fruitName){
+        cy.get(onDropdownCheckboxesRadiobuttonsPage.fruitDropdownList).find('option').contains(fruitName).then(fruit =>{
             const fruitValue = {
                 "Apple": "apple",
                 "Orange": "orange",
                 "Pear": "pear",
                 "Grape": "grape"
             }
-            if (index == 1) {
+            if (fruitName == 'Orange') {
                 cy.wrap(fruit).invoke('prop', 'value').should('contain', fruitValue[fruitName])
                 cy.wrap(fruit).invoke('prop', 'disabled').should('equal', true)
             } else {
-                cy.get('[class="container"]').contains('.thumbnail', 'Selected & Disabled').find(onDropdownCheckboxesRadiobuttonsPage.fruitDropdownList).select(fruitName)
+                cy.get(onDropdownCheckboxesRadiobuttonsPage.fruitDropdownList).select(fruitName)
                 cy.wrap(fruit).invoke('prop', 'selected').should('equal', true)
                 cy.wrap(fruit).should('contain', fruitName)
                 cy.wrap(fruit).invoke('prop', 'value').should('contain', fruitValue[fruitName])
@@ -72,59 +70,79 @@ export class dropdownCheckboxesRadiobuttonsPage{
             }
         })
     }
-
-    defaultCheckOfCheckboxes(){
-        cy.get('[class="container"]').contains('.thumbnail', 'Checkboxe(s)').find('[type="checkbox"]').each((checkBox, index) => {
-            if(index === 2){
-                cy.wrap(checkBox).should('be.checked')
-            }else{
-                cy.wrap(checkBox).should('not.be.checked')
+    checkNuberOfOptionsFirstDropdownList(number){
+        cy.get(onDropdownCheckboxesRadiobuttonsPage.javaDropdownList).find('option').should('have.length', number)
+    }
+    checkNuberOfOptionsSecondDropdownList(number){
+        cy.get(onDropdownCheckboxesRadiobuttonsPage.eclipseDropdownList).find('option').should('have.length', number)
+    }
+    checkNuberOfOptionsThirdDropdownList(number){
+        cy.get(onDropdownCheckboxesRadiobuttonsPage.htmlDropdownList).find('option').should('have.length', number)
+    }
+    checkNuberOfOptionsFruitDropdownList(number){
+        cy.get(onDropdownCheckboxesRadiobuttonsPage.fruitDropdownList).find('option').should('have.length', number)
+    }
+    // Check Boxy 
+    checkAndUncheckCheckBox(checkboxName){
+        cy.get(onDropdownCheckboxesRadiobuttonsPage.checkBoxesContainer).find('[value='+checkboxName+']').then(item => {
+            if(checkboxName === 'option-3'){
+                cy.wrap(item).click()
+                cy.wrap(item).should('not.be.checked')
+                cy.wrap(item).click()
+                cy.wrap(item).should('be.checked')
+            } else {
+                cy.wrap(item).click()
+                cy.wrap(item).should('be.checked')
+                cy.wrap(item).click()
+                cy.wrap(item).should('not.be.checked')
             }
-    })
+        })
     }
     checkAllCheckboxes(){
-        cy.get('[class="container"]').contains('.thumbnail', 'Checkboxe(s)').find('[type="checkbox"]').each((checkBox, index) => {
-            if (index !== 2) cy.wrap(checkBox).click({ force: true })
-            cy.wrap(checkBox).should('be.checked')
-        })
-    }
-    uncheckAllCheckboxes(){
-        cy.get('[class="container"]').contains('.thumbnail', 'Checkboxe(s)').find('[type="checkbox"]').each((checkBox) => {
-            cy.wrap(checkBox).click({ force: true })
-            cy.wrap(checkBox).should('not.be.checked')
-        })
-    }
-    uncheckTwoCheckboxes(checkboxesArray){
-        cy.get('[class="container"]').contains('.thumbnail', 'Checkboxe(s)').find('[type="checkbox"]').then(checkBoxes => {
-            cy.wrap(checkboxesArray).each(eqCheckbox => cy.wrap(checkBoxes).eq(eqCheckbox).click({ force: true }).should('not.be.checked'))
-        })
-    }
-    checkedPropertyCheckForAllRadioButtons(propValue){
-        cy.get('[class="container"]').contains('.thumbnail', 'Radio Button(s)').find('.section-title').find('[type="radio"]').each(item => {
-            if(propValue === true) cy.wrap(item).click()
-            cy.wrap(item).invoke('prop', 'checked').should('equal', propValue)
-        })
-    }
-    defaultCheckVegetableRadioButtons(){
-        cy.get('[class="container"]').contains('.thumbnail', 'Selected & Disabled').find('.section-title').find('[type="radio"]').each((item, index) => {
-            if (index < 2) {
-                cy.wrap(item).invoke('prop', 'checked').should('equal', false)
-            } else {
-                cy.wrap(item).invoke('prop', 'checked').should('equal', true)
+        cy.get(onDropdownCheckboxesRadiobuttonsPage.checkBoxesContainer).find('[type="checkbox"]').each(checkBox => {
+            if(!checkBox.is(':checked')){
+                cy.wrap(checkBox).click({ force: true })
+                cy.wrap(checkBox).should('be.checked')
             }
         })
     }
-    clickOnAllVegetableRadioButtons(){
-        cy.get('[class="container"]').contains('.thumbnail', 'Selected & Disabled').find('.section-title').find('[type="radio"]').each((item, index) => {
-            if (index == 0 || index == 2) {
-                cy.wrap(item).click()
-                cy.wrap(item).invoke('prop', 'checked').should('equal', true)
+    uncheckSelectedCheckboxes(checkboxesArray){
+        cy.get(onDropdownCheckboxesRadiobuttonsPage.checkBoxesContainer).find('label').then(checkBoxes => {
+            cy.wrap(checkboxesArray).each(checkboxName => cy.wrap(checkBoxes).find('[value='+checkboxName+']').click({ force: true }).should('not.be.checked'))
+        })
+    }
+    checkNuberOfCheckboxes(number){
+        cy.get(onDropdownCheckboxesRadiobuttonsPage.checkBoxesContainer).find('input').should('have.length', number)
+    }
+    //Radio Buttony 
+    clickRadioButton(buttonName){
+        cy.get(onDropdownCheckboxesRadiobuttonsPage.radioButtonsContainer).find('[value='+buttonName+']').then(item => {
+            cy.wrap(item).invoke('prop', 'checked').should('equal',false)
+        cy.wrap(item).click()   
+        cy.wrap(item).invoke('prop', 'checked').should('equal',true)
+        })
+    }
+    checkNuberOfRadioButtons(number){
+        cy.get(onDropdownCheckboxesRadiobuttonsPage.radioButtonsContainer).find('[type="radio"]').should('have.length', number)
+    }
+    clickVegetableRadiobuttons(buttonName){
+        cy.get(onDropdownCheckboxesRadiobuttonsPage.selectedDisabledRadioButtons).find('[value='+buttonName+']').then(item => {
+            if (buttonName === 'pumpkin'){ 
+                cy.wrap(item).invoke('prop', 'checked').should('equal',true)
             } else {
+                cy.wrap(item).invoke('prop', 'checked').should('equal',false)
+            }
+            if(buttonName === 'cabbage'){
                 cy.wrap(item).invoke('prop', 'disabled').should('equal', true)
-            }
+            } else {
+                cy.wrap(item).click()   
+                cy.wrap(item).invoke('prop', 'checked').should('equal',true)
+            }   
         })
     }
-
+    checkNuberOfOptionsVegetableRadioButtons(number){
+        cy.get(onDropdownCheckboxesRadiobuttonsPage.selectedDisabledRadioButtons).find('[type="radio"]').should('have.length', number)
+    }
 }
 
 export const onDropdownCheckboxesRadiobuttonsPage = new dropdownCheckboxesRadiobuttonsPage
